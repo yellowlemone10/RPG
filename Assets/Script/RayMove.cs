32056d7be30bf9d3bbc4f21d0d1988321b134ac5
DESKTOP-1D8UQ7K\stl <yellowlemone@naver.com>
2017-05-13 18:46:27 +0900

    네비게이션업데이트+달리기

--- Assets/Script/RayMove.cs
+++ Assets/Script/RayMove.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using UnityEngine.AI;
 public class RayMove : MonoBehaviour {
 
+    public bool isRun = false;
     public GameObject movePoint;
     public GameObject attackPoint;
+
+    public float moveSpeed = 4.0f;
+    public float turnSpeed = 360.0f;
+
 
     NavMeshAgent agent;
 
@@ -13,11 +18,19 @@ public class RayMove : MonoBehaviour {
     {
         movePoint.SetActive(false);
         attackPoint.SetActive(false);
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();   
+        agent.speed = moveSpeed;
+        agent.angularSpeed = turnSpeed;
+        agent.acceleration = 2000.0f;
     }
 
     void Update()
     {
+        if (agent.remainingDistance == 0)
+        {
+            isRun = false;
+            movePoint.SetActive(false);
+        }
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -29,6 +42,7 @@ public class RayMove : MonoBehaviour {
                 movePoint.transform.position = hitInfo.point;
                 movePoint.SetActive(true);
                 agent.SetDestination(movePoint.transform.position);
+                isRun = true;
             }
         }
     }
