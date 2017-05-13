8f8f93bfd7254e3e821e82f203e342391dd9b28b
DESKTOP-1D8UQ7K\stl <yellowlemone@naver.com>
2017-05-13 17:12:23 +0900

    RPG프로젝트 시작 koko메카닝 추가 마커 추가 지형 추가

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour {
+    public float moveSpeed = 4.0f;
+    public float turnSpeed = 360.0f;
+    public float gravityRario = 0.5f;
+
+    CharacterController cc;
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, turnSpeed * h * Time.deltaTime, 0);
+        cc.Move((transform.forward * v * moveSpeed+
+            Physics.gravity * gravityRario) * Time.deltaTime);
+    }
+}
