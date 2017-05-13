0cd26952394a91b03c43ee475584336b940ea517
DESKTOP-1D8UQ7K\stl <yellowlemone@naver.com>
2017-05-13 18:09:03 +0900

    네비게이션 및 이동 구현

---
+++ Assets/Script/RayMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class RayMove : MonoBehaviour {
+
+    public GameObject movePoint;
+    public GameObject attackPoint;
+
+    NavMeshAgent agent;
+
+   void Awake()
+    {
+        movePoint.SetActive(false);
+        attackPoint.SetActive(false);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if ( Physics.Raycast(ray, out hitInfo, 100.0f))
+            {
+                Debug.Log(hitInfo.point);
+                movePoint.transform.position = hitInfo.point;
+                movePoint.SetActive(true);
+                agent.SetDestination(movePoint.transform.position);
+            }
+        }
+    }
+}
